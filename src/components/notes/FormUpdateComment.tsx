e0fe00879d0d4d64344a579d4e8e408a4690c356
jordan <jordanelcracken@gmail.com>
2024-07-30 07:58:07 -0500

    Feat some ui form update one

--- src/components/notes/FormUpdateComment.tsx
+++ src/components/notes/FormUpdateComment.tsx
@@ -1,20 +1,21 @@
 "use client"
 import { updateOneNote } from '@/actions/crud'
+import { Loader } from 'lucide-react'
 import React, { useState } from 'react'
 
 const FormUpdateComment = ({ note, id }: { note: string, id: string }) => {
   const [noteUpdate, setNoteUpdate] = useState(note)
   const [statusLoading, setStatusLoading] = useState(false)
+  const [openNavard, setOpenNavbar] = useState(false)
 
   const updateNote = async (formData: FormData) => {
-    setStatusLoading(true)
-    await updateOneNote(id, formData.get('note') as string)
+    const { status } = await updateOneNote(id, formData.get('note') as string)
     setStatusLoading(false)
   }
   return (
     <div>
       <p className='tracking-tighter text-[13px]'>From DB  <span className='text-red-600'>{noteUpdate}</span></p>
-      <form action={updateNote} className='mt-2 flex flex-col'>
+      <form action={async (formData) => await updateNote(formData)} className='mt-2 flex flex-col gap-1'>
         <label htmlFor="name">Upadte Your Note</label>
         <input
           onChange={(e) => setNoteUpdate(e.target.value)}
@@ -23,6 +24,13 @@ const FormUpdateComment = ({ note, id }: { note: string, id: string }) => {
           id='name'
           className='text-black rounded-sm px-2 '
           type="text" />
+        <button
+          onClick={() => setStatusLoading(true)}
+          className='px-2 bg-sky-600 opacity-[1] disabled:scale-100 rounded-sm transition-all duration-150 hover:scale-105 disabled:opacity-[0.7] relative'
+        >
+          Submmit
+          <span className='animate-spin absolute right-0 mt-1'>{statusLoading ? <Loader size={12} /> : null}</span>
+        </button>
       </form>
     </div>
   )
