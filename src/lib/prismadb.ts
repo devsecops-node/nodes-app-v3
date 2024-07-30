b4cf6612eb2de0388b238b7ef317c7da1de8b19a
jordan <jordanelcracken@gmail.com>
2024-07-30 00:16:54 -0500

    Feat: 05

--- src/lib/prismadb.ts
+++ src/lib/prismadb.ts
@@ -1,20 +1,12 @@
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-import { PrismaClient } from "@prisma/client/extension";
 
-// const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-// export const prisma =
-//   globalForPrisma.prisma ||
-//   new PrismaClient({
-//     log: ['query'],
-//   })
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
 
-// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
-declare global {
-  var prisma: PrismaClient | undefined
-}
-
-export const prisma = globalThis.prisma || new PrismaClient()
-
-if (process.env.NODE_ENV !== "production") globalThis.print = prisma
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
